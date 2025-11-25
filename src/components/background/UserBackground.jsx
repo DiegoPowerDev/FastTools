ca3f4dbdb8e6e964aaf97dbe747ba41e804318c6
Diego Torres <diego_torres_11@hotmail.com>
2025-11-25 11:25:30 -0500

    añadiendo fondo para celular y fondos fixed

--- src/components/background/UserBackground.jsx
+++ src/components/background/UserBackground.jsx
@@ -3,14 +3,22 @@
 import { useFireStore } from "@/store/fireStore";
 
 export default function UserBackgroundImage() {
-  const { background } = useFireStore();
+  const { background, mobileBackground } = useFireStore();
   return (
-    <img
-      src={background}
-      alt={background}
-      className="absolute inset-0 w-full max-w-screen h-full object-contain opacity-40 -z-10 select-none pointer-events-none"
-      fetchPriority="high"
-      style={{ aspectRatio: "16/9" }}
-    />
+    <picture className="absolute inset-0 -z-10 pointer-events-none select-none">
+      {/* Mobile: se usará cuando el viewport sea <= 767px */}
+      <source media="(max-width: 767px)" srcSet={mobileBackground} />
+
+      {/* Desktop: cuando sea >= 768px */}
+      <source media="(min-width: 768px)" srcSet={background} />
+
+      {/* Fallback */}
+      <img
+        src={background}
+        alt={background}
+        className="fixed inset-0 w-full max-w-screen h-full object-contain opacity-40 -z-10 select-none pointer-events-none"
+        fetchPriority="high"
+      />
+    </picture>
   );
 }
